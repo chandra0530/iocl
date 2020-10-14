60f386349c668707f9e6038ce7ca0f5286a1dc41
chandra <chandrakantaharansingh.0@gmail.com>
2020-10-14 12:27:07 +0530

    changes

--- app/Http/Controllers/UserViewController.php
+++ app/Http/Controllers/UserViewController.php
@@ -10,76 +10,76 @@ class UserViewController extends Controller
     //
 
     public function home(){
-        Auth::guard('web');
-        if(auth()->id()){
+        // Auth::guard('web');
+        // if(auth()->id()){
             return view('user.home');
-        }else{
-            return view('user.login');
-        }
+        // }else{
+        //     return view('user.login');
+        // }
        
     }
     
     public function news(){
        
-        Auth::guard('web');
-        if(auth()->id()){
+        // Auth::guard('web');
+        // if(auth()->id()){
             return view('user.news');
-        }else{
-            return view('user.login');
-        }
+        // }else{
+        //     return view('user.login');
+        // }
     }
     public function news_detials(){
         
-        Auth::guard('web');
-        if(auth()->id()){
+        // Auth::guard('web');
+        // if(auth()->id()){
             return view('user.news_details');
-        }else{
-            return view('user.login');
-        }
+        // }else{
+        //     return view('user.login');
+        // }
     }
     public function events(){
         
-        Auth::guard('web');
-        if(auth()->id()){
+        // Auth::guard('web');
+        // if(auth()->id()){
             return view('user.events');
-        }else{
-            return view('user.login');
-        }
+        // }else{
+        //     return view('user.login');
+        // }
     }
     public function compititions(){
        
-        Auth::guard('web');
-        if(auth()->id()){
+        // Auth::guard('web');
+        // if(auth()->id()){
             return view('user.competitions');
-        }else{
-            return view('user.login');
-        }
+        // }else{
+        //     return view('user.login');
+        // }
     }
     public function ammouncement(){
        
-        Auth::guard('web');
-        if(auth()->id()){
+        // Auth::guard('web');
+        // if(auth()->id()){
             return view('user.announcement');
-        }else{
-            return view('user.login');
-        }
+        // }else{
+        //     return view('user.login');
+        // }
     }
     public function contact(){
        
-        Auth::guard('web');
-        if(auth()->id()){
+        // Auth::guard('web');
+        // if(auth()->id()){
             return view('user.contact');
-        }else{
-            return view('user.login');
-        }
+        // }else{
+        //     return view('user.login');
+        // }
     }
     public function videos(){
         
-        Auth::guard('web');
-        if(auth()->id()){
+        // Auth::guard('web');
+        // if(auth()->id()){
             return view('user.videos');
-        }else{
-            return view('user.login');
-        }
+        // }else{
+        //     return view('user.login');
+        // }
     }
 }
