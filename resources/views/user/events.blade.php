7e0f2bf06e8b9a6630d77ab073433b38b6621d95
chandra <chandrakantaharansingh.0@gmail.com>
2020-11-03 11:14:46 +0530

    changes

--- resources/views/user/events.blade.php
+++ resources/views/user/events.blade.php
@@ -36,7 +36,7 @@
 					</div> <!-- /.col -->
 					<div class="col-xs-12 col-sm-6 col-md-5">
 						<div class="skill-info">
-						<a href="event_details/{{$event->id}}">	<h3>{{$event->events_names}}</h3>
+						<a href="event_details/{{$event->id}}">	<h3 style="margin-top: 0;">{{$event->events_names}}</h3>
                           	<p  style="-webkit-line-clamp: 2 !Important;"> {!! html_entity_decode($event->event_details) !!}</p>
                               <!-- <div style="-webkit-line-clamp: 2 !Important;">
                               {!! html_entity_decode($event->event_details) !!}
