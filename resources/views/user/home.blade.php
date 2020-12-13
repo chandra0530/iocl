c7296686f481acb32a8a526ef82f48ef1127d462
chandra <chandrakantaharansingh.0@gmail.com>
2020-12-13 16:46:18 +0530

    changes

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -436,13 +436,13 @@
                                 <div class="row">
                                 <div class="col-md-6 hovernow" >
                                     <a href="/ushare"> 
-                                    <img src="{{ asset('assets/images/home_share.jpg')}}"  style="opacity: 0.5;" width="100%" height="100%">
+                                    <img src="{{ asset('assets/images/home_share.jpg')}}"   width="100%" height="100%">
                                      <h2 class="title header_back hide">USHARE</h2></a>
                                     </div>
 
                                     <div class="col-md-6 hovernow">
                                     <a href="/compotitions">
-                                    <img src="{{ asset('assets/images/home_compitition.jpg')}}" style="opacity: 0.5;" width="100%" height="100%">
+                                    <img src="{{ asset('assets/images/home_compitition.jpg')}}" width="100%" height="100%">
                                      <h2 class="title header_back  hide">COMPETITION</h2></a>
                                     </div>
                                    
