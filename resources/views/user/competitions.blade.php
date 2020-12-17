b1ba5f52f3f34c280980bde8527197e2067b3aa5
chandra <chandrakantaharansingh.0@gmail.com>
2020-12-17 23:28:18 +0530

    changes

--- resources/views/user/competitions.blade.php
+++ resources/views/user/competitions.blade.php
@@ -116,7 +116,7 @@
                         
                         </p>
                     </div>
-                    <div class="row">
+                    <div class="row" style="display:flex;position: relative;">
 									<div class="col-md-2">
 									<a href="#"><i class="fa fa-facebook" onclick="share_news_fb()"></i></a>
 									</div>
@@ -182,7 +182,7 @@
                         
                         </p>
                     </div>
-                    <div class="row">
+                    <div class="row" style="display:flex;position: relative;">
 									<div class="col-md-2">
 									<a href="#"><i class="fa fa-facebook" onclick="share_news_fb()"></i></a>
 									</div>
