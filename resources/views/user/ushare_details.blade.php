6adc0e4285c03c61ec1a43334923e2502f4d0e1d
Chandra <chandra@Chandras-MacBook-Air.local>
2021-02-05 01:07:31 +0530

    changes

--- resources/views/user/ushare_details.blade.php
+++ resources/views/user/ushare_details.blade.php
@@ -112,7 +112,7 @@
 													 <a href="#">{{$comment->created_at}}</a>
 												</h4>
 												@if($comment->user_id == Auth::user()->id)         
-<a href="/delete-ushare-comment/{{$comment->competition_id}}/{{$comment->user_id}}" class="btn btn-danger">Delete</a>
+<a href="/delete-ushare-comment/{{$comment->competition_id}}/{{$comment->user_id}}" class="btn btn-danger" style="float: right;">Delete</a>
 @endif
 												<p>{{$comment->ushare_comment}}</p>
 											</div> <!-- /.media-body -->
