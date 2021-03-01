25cb369713b8a8001d11658f34ef9c1943104e16
Chandra <chandra@Chandras-MacBook-Air.local>
2021-03-02 01:41:40 +0530

    minor changes

--- resources/views/admin/compititions/publishupdates.blade.php
+++ resources/views/admin/compititions/publishupdates.blade.php
@@ -15,7 +15,7 @@
           <div class="content-header-left col-md-9 col-12 mb-2">
             <div class="row breadcrumbs-top">
               <div class="col-12">
-                <h2 class="content-header-title float-left mb-0">All Compitition User Upload Request</h2>
+                <h2 class="content-header-title float-left mb-0">All Competition User Upload Request</h2>
                 <div class="breadcrumb-wrapper col-12">
                   <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="/admin">Home</a>
