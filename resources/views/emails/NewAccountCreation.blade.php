cd861befef3b5d2790ca60fcccebab5f6f777b3d
chandra0530 <43083370+chandra0530@users.noreply.github.com>
2021-03-04 16:51:10 +0530

    Update NewAccountCreation.blade.php

--- resources/views/emails/NewAccountCreation.blade.php
+++ resources/views/emails/NewAccountCreation.blade.php
@@ -2,3 +2,4 @@ Name: {{$details['name']}} <br>
 Email: {{$details['email']}}<br>
 Password: {{$details['password']}}<br>
 Employeid: {{$details['employeid']}}<br>
+Your account will be activated in next 72 hours.
