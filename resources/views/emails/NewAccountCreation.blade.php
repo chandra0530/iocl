2258d5ab78f137e88706c891afdd5dfc9c55eba2
Chandra <chandra@Chandras-MacBook-Air.local>
2021-03-06 00:07:00 +0530

    changes

--- resources/views/emails/NewAccountCreation.blade.php
+++ resources/views/emails/NewAccountCreation.blade.php
@@ -4,4 +4,4 @@ Password: {{$details['password']}}<br>
 Employeid: {{$details['employeid']}}<br>
 Thank you for for joining us . Your would be activated in 72 hrs .<br>
 Follow below mentioned link to access Parivaar site.<br>
-https://www.ioclparivaar.com
+http://www.ioclparivaar.com
