9af8d7d2ca5950f449737b601611b5c835f67eee
chandra0530 <43083370+chandra0530@users.noreply.github.com>
2021-03-10 00:55:47 +0530

    Update NewAccountCreation.blade.php

--- resources/views/emails/NewAccountCreation.blade.php
+++ resources/views/emails/NewAccountCreation.blade.php
@@ -2,6 +2,6 @@ Name: {{$details['name']}} <br>
 Email: {{$details['email']}}<br>
 Password: {{$details['password']}}<br>
 Employeid: {{$details['employeid']}}<br>
-Thank you for for joining us . Your would be activated in 72 hrs .<br>
+Thank you for for joining us . Your account will be activated in 72 hrs .<br>
 Follow below mentioned link to access Parivaar site.<br>
 http://www.ioclparivaar.com
